bd9867e07a6c3a4adac9ed1ab6ff923b12cce760
Jozef-Sz <joccopalica@gmail.com>
2020-08-03 10:14:19 +0200

    refactoring

--- SimplES.js
+++ SimplES.js
@@ -4,45 +4,106 @@ class SimplES {
             return SimplES.instance;
         }
 
+        // Event handler modules
         this.ClickEvent = new ClickEventsHandler();
         this.KeypressEvent = new KeypressEventsHandler();
 
         Object.freeze(this);
         SimplES.instance = this;
     }
+
+    static createXPathFromElement(element) {
+        const allNodes = document.getElementsByTagName('*');
+        let segs;
+        for (segs = []; element && element.nodeType == 1; element = element.parentNode) {
+            if (element.hasAttribute('id')) {
+                let uniqueIdCount = 0;
+                for (let n = 0; n < allNodes.length; n++) {
+                    if (allNodes[n].hasAttribute('id') && allNodes[n].id == element.id) uniqueIdCount++;
+                    if (uniqueIdCount > 1) break;
+                }
+                if (uniqueIdCount == 1) {
+                    segs.unshift(`id("${element.getAttribute('id')}")`);
+                    return segs.join('/');
+                } else {
+                    segs.unshift(`${element.localName.toLowerCase()}[@id="${element.getAttribute('id')}"]`);
+                }
+            }
+            else if (element.hasAttribute('class')) {
+                segs.unshift(`${element.localName.toLowerCase()}[@class="${element.getAttribute('class')}"]`);
+            } else {
+                let i, sib;
+                for (i = 1, sib = element.previousSibling; sib; sib = sib.previousSibling) {
+                    if (sib.localName == element.localName) i++;
+                }
+                segs.unshift(`${element.localName.toLowerCase()}[${i}]`);
+            }
+        }
+        return segs.length ? '/' + segs.join('/') : null;
+    }
+
+    static isFunction(func) {
+        return func && {}.toString.call(func) === '[object Function]';
+    }
+
+    static isElement(element) {
+        return (
+            typeof HTMLElement === "object" ? element instanceof HTMLElement : element &&
+            typeof element === "object" && element !== null && element.nodeType === 1 && typeof element.nodeName === "string"
+        );
+    }
 }
 
 
 class ClickEventsHandler {
     constructor() {
-        this.events = [];
+        // when possible change this.events and this.EMPTY_CLICK to private properties
+        this.events = {};
         this.EMPTY_CLICK = null;
         this.listen();
     }
 
     addEvent(element, func) {
-        const clickEvent = new ClickEvent(element, func);
-        this.events.push(clickEvent);
-        // return clickEvent;
+        if (!SimplES.isElement(element) && SimplES.isFunction(func)) {
+            throw `[Error]: An event can be created only with a pair of an element and a function, not a pair of ${typeof element} and ${typeof func}.`;
+        } else {
+            const XPath = SimplES.createXPathFromElement(element);
+            if (XPath in this.events) {
+                console.warn('Event you are attempting to create already exists.');
+                console.warn('Event with this element', element);
+                return;
+            }
+    
+            this.events[XPath] = new ClickEvent(element, func);
+            // TODO: considering to return the event
+            // return this.events[XPath];
+        }
+    }
+
+    addEmptyClick(func) {
+        if (SimplES.isFunction(func)) {
+            this.EMPTY_CLICK = func;
+        }
+        else {
+            throw `[Error]: Empty click event must be a funtion, not ${typeof func}!`;
+        }
     }
 
     listen() {
         const self = this;
         document.addEventListener('click', function(event) {
             const target = event.target;
-            let isTargetInEvents = false;
+            const XPath = SimplES.createXPathFromElement(target);
 
-            for (const clickEvent of self.events) {
-                const found = clickEvent.matchAndTrigger(target);
-                if (found) isTargetInEvents = true;
+            if (XPath in self.events) {
+                self.events[XPath].triggerFunction();
             }
-
-            if (!isTargetInEvents && self.EMPTY_CLICK) {
+            else if (self.EMPTY_CLICK) {
                 self.EMPTY_CLICK();
-            }
-            else if (!isTargetInEvents) {
+            } 
+            else {
                 console.warn('Empty click event has not been set!');
-                console.warn('You can add one with addEmptyClick() method.');
+                console.warn('You can add one with .ClickEvent.addEmptyClick() method.');
             }
         });
     }
@@ -60,14 +121,6 @@ class Event {
     constructor(func) {
         this.triggerFunction = func; 
     }
-
-    matchAndTrigger(target) {
-        if (target === this.targetElement) {
-            this.triggerFunction();
-            return true;
-        }
-        return false;
-    }
 }
 
 
@@ -82,4 +135,3 @@ class ClickEvent extends Event {
 class KeypressEvent extends Event {
     constructor() {}
 }
-
--- tests/User.js
+++ tests/User.js
@@ -3,3 +3,7 @@ const event_system = new SimplES();
 const title = document.getElementById('redone');
 
 event_system.ClickEvent.addEvent(title, () => console.log('Hey beauty, you clicked me!'));
+
+// event_system.ClickEvent.addEmptyClick("Hello");
+
+event_system.ClickEvent.addEvent("HEllo", () => console.log('Hey beauty, you clicked me!'));
