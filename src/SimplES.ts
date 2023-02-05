ead4700b35293df4254ff62e5702cb5d5c23f38c
Joco <37275518+szimeth-jozef@users.noreply.github.com>
2023-02-05 22:01:03 +0100

    Rewrite to typescript and create npm package

--- src/SimplES.ts
+++ src/SimplES.ts
@@ -1,29 +1,41 @@
 /**
- * @class SimplES constructor creates an event listener on the whole 
+ * Type for an event.
+ */
+export interface RawEventType {
+    element: HTMLElement,
+    onEvent: () => void
+}
+
+/**
+ * @class SimplES constructor creates an event listener on the whole
  * document, which after that listens to all of the clicks and compares
- * them to the given structures of elements and functions attached 
+ * them to the given structures of elements and functions attached
  * @constructor Accepts structures array of elements and functions see param
- * @param {Array<object>} rawPairs Array of objects, which contains 
+ * @param {Array<object>} rawPairs Array of objects, which contains
  * an element and a function [{element:x, onEvent:y},]
  */
-class SimplES {
-    constructor(rawPairs=null) {
-        this.events = [];
-        this.EMPTY_CLICK = null;
+export default class SimplES {
+    private readonly _events: Event[];
+    private _EMPTY_CLICK: (() => void) | null;
+
+    constructor(rawPairs: RawEventType[] | null = null) {
+        this._events = [];
+        this._EMPTY_CLICK = null;
         
-        if (rawPairs) this.attachFromConstructor(rawPairs);
+        if (rawPairs)
+            this.attachFromConstructor(rawPairs);
 
         this.listen();
     }
 
     /**
-     * @description Detects when there was click on a unregistred 
-     * element and triggers the EMPTY_CLICK event 
-     * @param {function} func Function attached to the event 
+     * @description Detects when there was click on a unregistred
+     * element and triggers the EMPTY_CLICK event
+     * @param {function} func Function attached to the event
      */
-    addEmptyClick(func) {
+    public addEmptyClick(func: () => void): void {
         if (isFunction(func)) {
-            this.EMPTY_CLICK = func;
+            this._EMPTY_CLICK = func;
         } else {
             throw "An event action must be a function!";
         }
@@ -35,11 +47,12 @@ class SimplES {
      * @param {function} func Function attached to the element
      * @returns An array of event objects for debug or a possible later use
      */
-    attachWithSameFunction(elements, func) {
-        const eventContainer = [];
+    public attachWithSameFunction(elements: HTMLElement[], func: () => void): Event[] {
+        const eventContainer: Event[] = [];
+
         for (const element of elements) {
             const event = new Event(element, func);
-            this.events.push(event);
+            this._events.push(event);
             eventContainer.push(event);
         }
         return eventContainer;
@@ -51,9 +64,9 @@ class SimplES {
      * @param {function} func Function attached to the element
      * @returns The event object for debug or a possible later use
      */
-    attachEvent(element, func) {
+    public attachEvent(element: HTMLElement, func: () => void): Event {
         const event = new Event(element, func);
-        this.events.push(event);
+        this._events.push(event);
         return event;
     }
 
@@ -61,14 +74,15 @@ class SimplES {
      * @description Attaches given events at the object creation
      * @param {Array} rawPairs Array of objects, which contains an element and a function
      */
-    attachFromConstructor(rawPairs) {
+    public attachFromConstructor(rawPairs: RawEventType[]): void {
         for (const pair of rawPairs) {
             if (!('element' in pair && 'onEvent' in pair)) {
                 console.error("You must specify an element key and an onEvent key in the object!");
                 return;
-            } 
+            }
+
             const event = new Event(pair.element, pair.onEvent);
-            this.events.push(event);
+            this._events.push(event);
         }
     }
 
@@ -76,20 +90,22 @@ class SimplES {
      * @description Creates an event listener on the document and it maches the events. It does not
      * need a call externaly, it's called from the contructor
      */
-    listen() {
+    private listen(): void {
         const self = this;
+
         document.addEventListener('click', function(event) {
-            const target = event.target;
+            const target = event.target as HTMLElement;
             let isTargetInEvents = false;
 
-            for (const event of self.events) {
+            for (const event of self._events) {
                 const found = event.match(target);
                 if (found) isTargetInEvents = true;
             }
 
-            if (!isTargetInEvents && self.EMPTY_CLICK) {
-                self.EMPTY_CLICK();
+            if (!isTargetInEvents && self._EMPTY_CLICK) {
+                self._EMPTY_CLICK();
             }
+
             else if (!isTargetInEvents) {
                 console.warn("Empty click event has not been set! You can add one with addEmptyClick() method.");
             }
@@ -98,15 +114,18 @@ class SimplES {
 }
 
 class Event {
-    constructor(elem, func) {
-        this.trackedElement = elem;
-        this.eventFunction = func;
+    private readonly _trackedElement: HTMLElement;
+    private readonly _eventFunction: () => void;
+
+    constructor(elem: HTMLElement, func: () => void) {
+        this._trackedElement = elem;
+        this._eventFunction = func;
         // this.eventID = this.generateID();
     }
 
-    match(target) {
-        if (target === this.trackedElement) {
-            this.eventFunction();
+    public match(target: HTMLElement): boolean {
+        if (target === this._trackedElement) {
+            this._eventFunction();
             return true;
         }
         return false;
@@ -116,11 +135,11 @@ class Event {
 
 // TODO: use these functions to test input parameter of methods
 
-function isFunction(functionToCheck) {
+function isFunction(functionToCheck: any): boolean {
     return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
 }
 
-function isElement(o) {
+function isElement(o: any): boolean {
     return (
       typeof HTMLElement === "object" ? o instanceof HTMLElement : //DOM2
       o && typeof o === "object" && o !== null && o.nodeType === 1 && typeof o.nodeName==="string"
